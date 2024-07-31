6cfbe0c0da6464cf3994ba163b4972da1c0eb9f7
thiago.felizardo <thiagofelizardo2@gmail.com>
2024-07-31 19:29:37 -0300

    removendo classes

--- src/main/java/com/phoebus/s3/S3Provider.java
+++
@@ -1,7 +0,0 @@
-package com.phoebus.s3;
-
-import io.micronaut.objectstorage.ObjectStorageOperations;
-
-public abstract class S3Provider {
-    public abstract ObjectStorageOperations getOperations();
-}
