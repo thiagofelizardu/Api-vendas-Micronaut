53c3bb892d5773633b6e7665007f525dadb15afe
Thiago Felizardo <thiagofelizardo2@gmail..com>
2025-03-08 14:12:21 -0300

    adicionando exceção global

--- src/main/java/com/phoebus/model/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phoebus/model/exception/GlobalExceptionHandler.java
@@ -3,23 +3,24 @@ package com.phoebus.model.exception;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
-import io.micronaut.http.annotation.Produces;
-import io.micronaut.http.server.exceptions.ExceptionHandler;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
 import jakarta.inject.Singleton;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Produces
 @Singleton
-public class GlobalExceptionHandler implements ExceptionHandler<Exception, HttpResponse<?>> {
+@Controller
+public class GlobalExceptionHandler {
 
-    @Override
-    public HttpResponse<?> handle(HttpRequest request, Exception exception) {
+    @Error(global = true)
+    public HttpResponse<?> handle(HttpRequest<?> request, Exception exception) {
         Map<String, Object> response = new HashMap<>();
         response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.getCode());
         response.put("error", "Erro interno no servidor");
         response.put("message", exception.getMessage());
+
         return HttpResponse.serverError(response);
     }
 }
